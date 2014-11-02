dedfbb13da29a6aae0519eb425dde9ddae4c0270
Jiateng <327540310@qq.com>
2014-11-02 16:21:21 +0800

    diff testing

--- js/function.js
+++ js/function.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var v1,v2,sm;
     $(document).on("keypress","code",function(event){
 	console.log(event.which);
 	console.log(this.innerHTML);
@@ -8,14 +9,23 @@ $(document).ready(function(){
 	    $("<code contenteditable ='true'></code>").appendTo("pre");
 	}
     });
-    
+  
+    /*  
     $("#editor").keypress(function(event){
 //	console.log(event.which);
 	if(event.which == 13){
 	    console.log();
 //t	    console.log(Object.keys(editor.getCursorPosition()));
 	    console.log(editor.getSession().getLine(editor.getCursorPosition().row));
-
 	}
     });
+    */
+
+    editor.on("change",function(e){
+	//return "data"	
+	//console.log(Object.keys(e));
+	console.log(e.data);
+    });
+
+//    $("#snapButton").onclick
 });
