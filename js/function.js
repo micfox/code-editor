fd4e13c59149f00cc8d1f81550f40cd1011ee6db
Jiateng <327540310@qq.com>
2014-11-08 18:06:32 +0800

    sht+enter to submit /post-line

--- js/function.js
+++ js/function.js
@@ -11,10 +11,27 @@ $(document).ready(function(){
     
     $("#editor").keypress(function(event){
 //	console.log(event.which);
-	if(event.which == 13){
-	    console.log();
-//t	    console.log(Object.keys(editor.getCursorPosition()));
-	    console.log(editor.getSession().getLine(editor.getCursorPosition().row));
+	if(event.shiftKey && (event.which == 13)){
+//	    console.log(Object.keys(editor.getCursorPosition()));
+//	    console.log(editor.getSession().getLine(editor.getCursorPosition().row));
+	          $.ajax({
+	  url: '/post-line',
+	  type: 'POST',
+	  dataType: 'html',
+	  data: editor.getSession().getLine(editor.getCursorPosition().row),
+	  beforeSend: function(){
+//	      console.log("before send");
+	  },
+	  success: function(data){
+	      alert.html(data).fadeIn();
+	      //form.tirgger('reset');
+	      submit.html('Send Email');
+	      console.log(data);
+	  },
+	  error: function(e){
+	      console.log(e);
+	  },
+      });
 
 	}
     });
